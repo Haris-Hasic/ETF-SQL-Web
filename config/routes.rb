de2251f2935fac4ae0a40e0fc7752050e23a4fc7
aldinhasanspahic <ahasanspah1@gmail.com>
2015-10-26 20:18:07 +0000

    Struktura stranice

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'settings/index'
+
+  get 'workspace/index'
+
+  get 'register/index'
+
+  get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root ''
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
