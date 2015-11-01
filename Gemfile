02fa346040add7590511ac3b0eb0fb5d8f9cfa9e
kenanekinovic <kekinovic1@etf.unsa.ba>
2015-11-01 16:34:24 +0000

    Dodani useri pomocu  devise

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+#Devise - user
+gem 'devise'
 #AngularJS i Bootstrap
 gem 'angularjs-rails'
 gem 'bootstrap-sass'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  #has_secure_password #
   has_one :preference
   has_many :user_histories
 end
