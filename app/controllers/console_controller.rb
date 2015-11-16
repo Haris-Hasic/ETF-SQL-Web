844f4fa9048dd5e9b8f51eda34a7eeea58e97242
aldinhasanspahic <ahasanspah1@gmail.com>
2015-11-16 22:03:41 +0000

    Popravljen bug sa prevodom.

--- app/controllers/console_controller.rb
+++ app/controllers/console_controller.rb
@@ -1,6 +1,5 @@
 class ConsoleController < ApplicationController
   def index
-    I18n.locale = :bs
   	@queryresult = 'The result of your query will be here!'
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,10 @@ module Etfsql
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
-     #config.i18n.default_locale = :'bs'
-     # I18n.locale = :bs
+    I18n.config.available_locales = :en
+    I18n.config.available_locales = :bs
+     config.i18n.default_locale = 'bs'
+     #I18n.locale = :bs
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
